7e7adc7a7248b2ffe0575734010ddc49601bb026
Atal Sharma <atals3923@gmail.com>
2026-03-20 11:42:00 +0000

    container config done

---
+++ dem_01/03_dataTy.js
@@ -0,0 +1,16 @@
+console.log("hello")
+
+let name="allen"//string
+let age=90 // number
+let male=true // boolean
+let sal=90.22   //number
+let date// undefined
+//console.log([name,age,male,sal,date])
+console.table([name,age,male,sal,date])
+console.log(typeof name)
+console.log(typeof age)
+console.log( typeof male)
+console.log(typeof sal)
+console.log(typeof date)
+console.log(typeof null)// object and "null is a primitive value, but typeof null returns 'object' due to a historical bug in JavaScript."
+console.log(typeof undefined)
