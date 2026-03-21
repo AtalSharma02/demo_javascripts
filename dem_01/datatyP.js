449721115911ee7e99df21ec37f941c5d3b46f0c
Atal Sharma <atals3923@gmail.com>
2026-03-21 13:12:16 +0000

    data type done

---
+++ dem_01/datatyP.js
@@ -0,0 +1,22 @@
+// primitive 
+
+// let name = "Atal";     // string
+// let age = 22;         // number
+// let isOk = true;      // boolean
+// let x;                // undefined
+// let y = null;         // null
+// let big = 123n;       // bigint
+// let sym = Symbol();   // symbol
+//Symbol → uniqueness
+// BigInt → precision limit
+// console.log(name,age,isOk,x,y,big,sym)
+
+//non-primitive or refernce
+
+// let obj = { name: "Atal" }; // object
+// let arr = [1, 2, 3];        // array
+// let fun = function() {};    // function
+// console.log(obj,arr,fun)
+
+// "Primitive types are immutable and stored by value,
+//  while non-primitive types are mutable and stored by reference."
