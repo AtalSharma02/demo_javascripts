627ae5da533a89a653f47365c604cbdaf6ed0425
Atal Sharma <atals3923@gmail.com>
2026-03-22 11:45:33 +0000

    memory

--- dem_01/datatyP.js
+++ dem_01/datatyP.js
@@ -20,3 +20,26 @@
 
 // "Primitive types are immutable and stored by value,
 //  while non-primitive types are mutable and stored by reference."
+
+//----------------------------Heap and Stack---------------------------------------------
+
+
+// stack[primivite]
+
+// let a=10
+// let b=a
+// b=20// independent 
+// agar b change hua to a change nhi hoga kyu store by value
+// console.log(a)//10
+// console.log(b)// 20
+
+// heap[non-primitive ]
+
+//  let obj={
+//     name:"allen"
+//  }
+//  let b=obj// copy by reference
+//  b.name="king"
+// ager b change hoga to obj bhi change ho jayega kyu ki ye store by reference[share]
+//  console.log(obj)// king
+//  console.log(b)// king
